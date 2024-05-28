d492939434a5aa235382ed5177d4005556f88b57
Hyso <hsk7953@gmail.com>
2024-05-28 16:09:16 +0900

    ArrayList 순서 뒤집기

--- Java_sol/Java_basic/ArrayListEx1.java
+++ Java_sol/Java_basic/ArrayListEx1.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 class ArrayListEx1{
     public static void main(String[] args){
-        ArrayList list1 = new ArrayList(10);
-        
+        // ArrayList list1 = new ArrayList(10);
+        Integer[]arr = {1,2,3,4,5,6};
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println(Arrays.toString(arr));
     }
 }
