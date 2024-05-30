5c16eb12c4ae60aa091bd4792be3bd66b3496391
Hyso <hsk7953@gmail.com>
2024-05-30 18:38:22 +0900

    주석 추가

--- Java_sol/Java_basic/doit_algorithm/chap02/CloneArray.java
+++ Java_sol/Java_basic/doit_algorithm/chap02/CloneArray.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class CloneArray {
     public static void main(String[] args){
         int[] a = {1,2,3,4,5};
-        int[] b = a.clone();
+        int[] b = a.clone(); // b는 a의 복제를 참조. 
 
         b[3] = 0;
         System.out.println("a = " + Arrays.toString(a));
