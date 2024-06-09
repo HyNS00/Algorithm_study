c6e5fdc5dfa6c97c8f9cd4f9ab1e95487e325b51
Hyso <hsk7953@gmail.com>
2024-06-09 18:24:29 +0900

    🐛 변수 변경

--- Java_sol/Java_basic/doit_algorithm/chap05/Hanoi.java
+++ Java_sol/Java_basic/doit_algorithm/chap05/Hanoi.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Hanoi {
     static void move(int no, int x, int y){
         if(no >1){
-            move(n-1,x,6-x-y);
+            move(no-1,x,6-x-y);
         }
         System.out.printf("원반[%d]을(를) %d번 기둥에서 %d 기둥으로 옮김\n", no, x,y);
 
         if(no > 1){
-            move(n-1,6-x-y, y);
+            move(no-1,6-x-y, y);
         }
     }
 
