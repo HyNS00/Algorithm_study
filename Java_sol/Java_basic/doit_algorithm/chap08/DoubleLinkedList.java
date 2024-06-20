d1c8634305fcd9d5121792a9e02f30cb3a3de5aa
Hyso <hsk7953@gmail.com>
2024-06-20 10:56:10 +0900

    나머지 구현

--- Java_sol/Java_basic/doit_algorithm/chap08/DoubleLinkedList.java
+++ Java_sol/Java_basic/doit_algorithm/chap08/DoubleLinkedList.java
@@ -44,5 +44,106 @@ public class DoubleLinkedList<E> {
         }
         return null; // 검색 실패
     }
-   
+    // 선택 노드를 출력
+    public void printCurrentNode(){
+        if(isEmpty()){
+            System.out.println("선택 노드가 없습니다.");
+        }
+        else{
+            System.out.println(crnt.data);
+        }
+    }
+
+    public void dump(){
+        Node<E> ptr = head.next;
+        while(ptr != head){
+            System.out.println(ptr.data);
+            ptr = ptr.next;
+        }
+    } // 모든 노드 출력
+    
+    public void dumpReverse(){
+        Node<E> ptr = head.prev;
+
+        while(ptr != head){
+            System.out.println(ptr.data);
+            ptr = ptr.prev;
+        }
+    } // 반대로 출력
+
+    // 선택 노드를 앞쪽으로 진행
+    public boolean prev(){
+        if(isEmpty() || crnt.prev == head){
+            return false;
+        }
+        crnt = crnt.prev;
+        return true;
+    }
+
+    public void add(E obj){
+        Node<E> node new Node<E> (obj, crnt, crnt.next);
+        crnt.next = crnt.next.prev = node; // 사이에 집어 넣는 것
+        // 당연한거지만 오른쪽에서 왼쪽으로 생각한다.
+        // crnt.prev | crnt | crnt.next 순으로 생각
+        crnt = node;
+    }
+
+    // 머리에 노드를 삽입
+    public void addFirst(E obj){
+        crnt = head;
+        add(obj);
+    }
+
+    // 꼬리에 노드를 삽입
+    public void addLast(E obj){
+        crnt = head.prev; // 원형이기에 꼬리의 전 노드는 head다
+        add(obj);
+    }
+
+    // 선택 노드를 삭제
+    public void removeCurrentNode(){
+        if(!isEmpty()){
+            crnt.prev.next=  crnt.next;
+            crnt.next.prev = crnt.prev;
+            crnt = crnt.prev;
+            if(crnt == head){
+                crnt = head.next;
+            }
+        }// crnt.prev | crnt | crnt.next 순으로 존재하니까
+        // crnt가 사라지고 crnt.prev의 다음 노드는 crnt.next가 된다 -> crnt.prev.next = crnt.next
+        // crnt가 사라지니 crnt.next의 전 노드는 crnt.prev가 된다 -> crnt.next.prev = crnt.prev;
+    }
+
+    // 노드를 삭제
+    public void remove(Node p){
+        Node<E>  ptr = head.next;
+        while(ptr != head){
+            if(ptr == p){
+                crnt = p;
+                removeCurrentNode();
+                break;
+            }
+            ptr= ptr.next;
+        }
+    }
+
+    // 머리 노드를 삭제
+    public void removeFirst(){
+        crnt = head.next; // 첫 번째 노드 말고 더미노드인 head가 존재
+        // 단순히 head.next를 변경해줌으로써옮겨준다.
+        removeCurrentNode();
+    }
+
+    // 꼬리 노드를 삭제
+    public void removeLast(){
+        crnt = head.prev;
+        removeCurrentNode();
+    }
+
+    // 모든 노드를 삭제
+    public void clear(){
+        while(!isEmpty()){
+            removeFirst();
+        }
+    }
 }
